e7c93fff470166215a07cfa6ae544baebb13d0c5
ghelpo <m.f.ragab5890@gmail.com>
2020-11-22 14:55:46 +0200

    add_models/modify_app
    separated file models.py from app and modify app.
    models show foreign key added to artist/venue id

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from sqlalchemy import func
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
+from models import *
 from forms import *
 from flask_migrate import Migrate
 
@@ -22,73 +23,13 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
 
+db.init_app(app)
 # create instance migrate for data migration
 migrate = Migrate(app, db)
 
 
 # connection in configuration file added
-
-# ----------------------------------------------------------------------------#
-# Models.
-# ----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    genre = db.Column(db.String(), nullable=False)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    def __repr__(self):
-        return f'<Venue {self.id} {self.name}>'
-
-
-# add columns to db table venue
-Venue.seeking_talent = db.Column(db.Boolean, default=False)
-Venue.seeking_description = db.Column(db.String())
-Venue.website = db.Column(db.String())
-
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    genres = db.Column(db.String(120), nullable=False)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    def __repr__(self):
-        return f'<Artist {self.id} {self.name}>'
-
-
-# add columns to db table venue
-Artist.seeking_venue = db.Column(db.Boolean, default=False)
-Artist.seeking_description = db.Column(db.String())
-Artist.website = db.Column(db.String())
-
-#add show table
-class Show(db.Model):
-    __tablename__ = 'Show'
-    id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, nullable=False)
-    venue_id = db.Column(db.Integer, nullable=False)
-    start_time = db.Column(db.DateTime(), nullable=False)
-
-    def __repr__(self):
-        return f'<Show {self.id} {self.artist_id} {self.venue_id}>'
-
 # ----------------------------------------------------------------------------#
 # Filters.
 # ----------------------------------------------------------------------------#
---
+++ models.py
@@ -0,0 +1,65 @@
+from flask_sqlalchemy import SQLAlchemy
+
+#intiate db with no assigment
+db = SQLAlchemy()
+
+
+# ----------------------------------------------------------------------------#
+# Models.
+# ----------------------------------------------------------------------------#
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genre = db.Column(db.String(), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name}>'
+
+
+# add columns to db table venue
+Venue.seeking_talent = db.Column(db.Boolean, default=False)
+Venue.seeking_description = db.Column(db.String())
+Venue.website = db.Column(db.String())
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name}>'
+
+
+# add columns to db table venue
+Artist.seeking_venue = db.Column(db.Boolean, default=False)
+Artist.seeking_description = db.Column(db.String())
+Artist.website = db.Column(db.String())
+
+#add show table
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime(), nullable=False)
+
+    def __repr__(self):
+        return f'<Show {self.id} {self.artist_id} {self.venue_id}>'
+
